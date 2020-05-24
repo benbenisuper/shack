72653bd5fbc2dc0e684f531553a5f9cb0d434240
Morgan Hoare <hoaremorgan@gmail.com>
2020-05-24 18:56:03 -0300

    feat[admin]: Install active admin

---
+++ app/admin/bookings.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Booking do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :user_id, :start_date, :end_date, :status, :venue_id, :price
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:user_id, :start_date, :end_date, :status, :venue_id, :price]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
---
+++ app/admin/users.rb
@@ -0,0 +1,60 @@
+ActiveAdmin.register User do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :phone, :password, :password_confirmation, :avatar
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :first_name, :last_name, :phone]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+
+  index do
+    selectable_column
+    id_column
+    column :avatar
+    column :email
+    column :first_name
+    column :last_name
+    column :phone
+    actions
+  end
+
+  form title: 'New User' do |f|
+    f.inputs "User" do
+      f.input :first_name,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "First Name" }
+      f.input :last_name,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "Last Name" }
+      f.input :phone,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "Phone Number" }
+      f.input :email,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "email" }
+      f.input :password,
+      required: true,
+      hint: ("#{@minimum_password_length} characters minimum" if @minimum_password_length),
+      input_html: { autocomplete: "new-password" } 
+      f.input :password_confirmation,
+      required: true,
+      input_html: { autocomplete: "new-password" } 
+
+      f.input :avatar, as: :file, label: "Avatar (optional)"
+    end
+    para "Press cancel to return to the list without saving."
+    actions
+  end
+end
