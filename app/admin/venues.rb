a5e60df8870c3e2394c693bebfceb7bffedd212e
Morgan Hoare <hoaremorgan@gmail.com>
2020-05-31 12:19:33 -0300

    feat[venue]: Add permitted params

--- app/admin/venues.rb
+++ app/admin/venues.rb
@@ -5,7 +5,7 @@ ActiveAdmin.register Venue do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :name, :location, :user_id, :category, :description, :capacity, :activity, :price, :latitude, :longitude
+  permit_params :name, :location, :user_id, :category, :description, :capacity, :activity, :price, :latitude, :longitude
   #
   # or
   #
@@ -14,5 +14,35 @@ ActiveAdmin.register Venue do
   #   permitted << :other if params[:action] == 'create' && current_user.admin?
   #   permitted
   # end
-  
+  form title: 'New User' do |f|
+    f.inputs "User" do
+      f.input :first_name,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "First Name" }
+      f.input :last_name,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "Last Name" }
+      f.input :phone,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "Phone Number" }
+      f.input :email,
+      required: true,
+      autofocus: true,
+      input_html: { autocomplete: "email" }
+      f.input :password,
+      required: true,
+      hint: ("#{@minimum_password_length} characters minimum" if @minimum_password_length),
+      input_html: { autocomplete: "new-password" } 
+      f.input :password_confirmation,
+      required: true,
+      input_html: { autocomplete: "new-password" } 
+
+      f.input :avatar, as: :file, label: "Avatar (optional)"
+    end
+    para "Press cancel to return to the list without saving."
+    actions
+  end
 end
