190e32193c05c7d8043d87021684b477b56dcc34
Morgan Hoare <hoaremorgan@gmail.com>
2020-06-15 04:52:30 -0300

    Change migration

--- db/migrate/20200614220356_add_amount_to_bookings.rb
+++ db/migrate/20200614220356_add_amount_to_bookings.rb
@@ -1,4 +1,4 @@
-class AddPriceToBookings < ActiveRecord::Migration[6.0]
+class AddAmountToBookings < ActiveRecord::Migration[6.0]
   def change
   	add_monetize :bookings, :amount, currency: { present: false }
   end
