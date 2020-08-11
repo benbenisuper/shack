8f8ae17834c90db7b0a4cd546fdeb35109322a09
Morgan Hoare <hoaremorgan@gmail.com>
2020-08-11 17:01:04 -0300

    fix[API]: fix API

--- app/controllers/api/v1/calendars_controller.rb
+++ app/controllers/api/v1/calendars_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::CalendarsController < ApplicationController
+class Api::V1::CalendarsController < Api::V1::BaseController
   before_action :get_calendar
   skip_before_action :authenticate_user!, only: [:show]
 
