afc511d586c4d09966124ccfc2a742bc2286a608
Morgan Hoare <hoaremorgan@gmail.com>
2020-09-22 15:19:00 -0300

    Fix no messages bug

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,6 +10,8 @@ class PagesController < ApplicationController
     @venues = current_user.venues.select{ |v| v.upcoming_checkin_date[:date] }
     @next_checkin = current_user.next_checkin
     @next_checkout = current_user.next_checkout
+    @latest_message_booking = "#"
+    @latest_message_booking = booking_path(current_user.all_messages.first.chat_box.booking) unless current_user.all_messages.empty?
   end
 
 end
