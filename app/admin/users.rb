1996f7886dbe8fbaee8d5e4dd32f32b5bd663a3e
Morgan Hoare <hoaremorgan@gmail.com>
2020-09-28 00:30:31 -0300

    Admin user columns

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -18,11 +18,12 @@ ActiveAdmin.register User do
   index do
     selectable_column
     id_column
-    column :avatar
     column :email
     column :first_name
     column :last_name
     column :phone
+    column :uid
+    column :publishable_key
     actions
   end
 
